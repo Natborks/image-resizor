6ff83b3166edd2765a7e66753166c8b230c60a78
Ifedayo Ijabadeniyi <ifedayoadeniyi@gmail.com>
2020-06-09 12:14:26 +0100

    Validation Script added

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -8,6 +8,13 @@ class ImageController extends Controller
 {
     //
     public function resize(Request $request){
+        
+        $validatedData = $request->validate([
+            
+            ‘image’ => ‘bail|required|mimes:jpeg, bmp, png|size:5000|dimensions:min_width=100, min_height=100, 
+            max_height=2000, max_width=3000’,
+            
+            ‘resize’ => ‘required’])
 
         return response()->json([
             'message'  => 'resize method hit'

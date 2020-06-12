ee0f2509fb2c79730cf8068fbfa9fd1a02a2ab16
klickers <bluebirid228@gmail.com>
2020-06-12 07:25:08 -0700

    update documentation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
             <div class="col-lg-8 mx-auto">
             <h2>About this application</h2>
             <p class="lead">This is a microservice application for image resizing.
-                 The documentation is below, but you can find more api documentation at our
+                 Scroll down for the documentation, or go to our
                    <a href="https://app.swaggerhub.com/apis-docs/Team-thanos/Team-thanos/0.0.1-oas3#/">Swagger docs</a>.
             </p>
             </div>
@@ -29,31 +29,33 @@
         <div class="container">
         <div class="row">
             <div class="col-lg-8 mx-auto">
-            <h2>Usage</h2>
-            <p class="lead">
-                Send a POST request to
-                <a href="#">https://imageresize.microapi.dev/v1/upload</a> with the following payload: <br>
-                <pre>
-                payload:
-                {
-                “image”: “file”,
-                “width”: 0,
-                “height”: 0,
-                }</pre>
-                <br>
-            </p>
-            <p class = "lead">
-                Description: This endpoint takes an image in parameters, and resizes according to the height and width provided.
-                Note that the image should not be more than 2mb, and the width and height parameters should be between 0 and 10001.
-
-                <br>
-                <br>
-                Send a GET request to
-                <a href="#">https://imageresize.microapi.dev/v1/download/{filename}</a><br>
-                Makes file available for download at the specified url
-                <br>
-                Description: This endpoint makes the resized image available for download at the specified url
-            </p>
+                <h2>Usage</h2>
+                <div class = "api">
+                    <p class="lead">
+                        Send a POST request to
+                        <a href="#">https://imageresize.microapi.dev/v1/upload</a> with the following payload: <br>
+                        <pre>
+                        payload:
+                        {
+                            “image”: “file”,
+                            “width”: 0,
+                            “height”: 0,
+                        }</pre>
+                    </p>
+                    <p class = "lead">
+                        Description: This endpoint takes an image in parameters, and resizes according to the height and width provided.
+                        Note that the image should not be more than 2mb, and the width and height parameters should be between 0 and 10001.
+                    </p>
+                </div>
+                <div class = "api">
+                    <p class = "lead">
+                        Send a GET request to
+                        <a href="#">https://imageresize.microapi.dev/v1/download/{filename}</a><br>
+                        Makes file available for download at the specified url
+                        <br>
+                        Description: This endpoint makes the resized image available for download at the specified url
+                    </p>
+                </div>
             </div>
         </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/documentation', 'MainController@documentation')->name('docs');
